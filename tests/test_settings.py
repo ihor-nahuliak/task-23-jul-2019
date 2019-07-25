8a8a6f88ce9e6cc5145c069ff15323093459a084
ihor-nahuliak <ihor.nahuliak@gmail.com>
2019-07-25 22:07:56 +0200

    tests: settings

---
+++ tests/test_settings.py
@@ -0,0 +1,23 @@
+import unittest
+
+from quicksets import settings
+
+
+class TestCase(unittest.TestCase):
+
+    def test_root_path_is_absolute(self):
+        self.assertTrue(settings.ROOT_PATH.startswith('/'))
+
+    def test_root_path_is_app_dir(self):
+        self.assertTrue(settings.ROOT_PATH.endswith('/app'))
+
+    def test_postgresql_url(self):
+        self.assertEqual(
+            settings.POSTGRESQL_URL,
+            'postgresql+psycopg2://postgres:@localhost:5432'
+            '/task23jul2019_testing'
+        )
+
+
+if __name__ == '__main__':
+    unittest.main()

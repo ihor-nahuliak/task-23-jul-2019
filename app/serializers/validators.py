f821921a9a0a50e918ee8993d10dd523f72b993e
ihor-nahuliak <ihor.nahuliak@gmail.com>
2019-07-26 16:50:08 +0200

    serializers: wallet schema

--- app/serializers/validators.py
+++ app/serializers/validators.py
@@ -8,3 +8,5 @@ client_name_validator = vld.Length(min=1, max=160)
 country_name_validator = vld.Length(min=1, max=40)
 
 city_name_validator = vld.Length(min=1, max=80)
+
+currency_validator = vld.OneOf(['USD', 'EUR', 'CAD', 'CNY'])
